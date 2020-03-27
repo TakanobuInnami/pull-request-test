03402d62209b94cc8420056b649af9f9eb3c3313
innami <test@test.com>
2020-03-27 17:07:12 +0900

    社員一覧表作成

--- List.php
+++ List.php
@@ -6,6 +6,16 @@
 <h1>社員一覧画面</h1>
 </head>
 <body>
+<table border="1">
+  <tr>
+    <th>社員ID</th>
+    <th>社員名</th>
+    <th>所属セクション</th>
+    <th>メールアドレス</th>
+    <th>性別</th>
+  </tr>
+</table>
 
+<p><a href="http://localhost/Directory/Directory.php">メニュー画面</a></p>
 </body>
 </html>
