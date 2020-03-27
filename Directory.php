f542b66bde6c5cd7f99192e72ef4552597aebe5b
innami <test@test.com>
2020-03-27 16:45:05 +0900

    first commit

---
+++ Directory.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>社員名簿</title>
+<h1>社員名簿Web管理システム</h1>
+</head>
+<body>
+<p>管理メニュー</p>
+<ul>
+<li><a href="">社員登録画面</a> </li>
+<li><a href="">社員一覧画面</a></li>
+</ul>
+</body>
+</html>
