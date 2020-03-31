24b9f52e908a9e5b974ad76a9160bb1494a5ab90
innami <test@test.com>
2020-03-31 13:04:00 +0900

    データベース接続部分追記

--- Registory.php
+++ Registory.php
@@ -1,12 +1,54 @@
-<?php //サーバーサイドの入力チェック
+<?php 
+  error_reporting(E_ALL & ~E_NOTICE);
+  //データベース接続
+  require_once("join.php");
+  //サーバーサイドの入力チェック
   if (isset($_POST)){
-    $str = mb_strlen($_POST["id"]);
+    //文字の長さ計算
+    $id_len = mb_strlen($_POST['id']);
+    $ln_len = mb_strlen($_POST['lastname']);
+    $fn_len = mb_strlen($_POST['firstname']);
+    $mail_len = mb_strlen($_POST['mailaddress']);
+
+    //IDのチェック
     if(empty($_POST['id'])){
-      echo "IDは必ず入力してください";
-    }elseif($str > 10){
-      echo "文字数は10文字以内で入力してください";
+      echo "IDは必ず入力してください<br>";
+    }elseif($id_len > 10){
+      echo "文字数は10文字以内で入力してください<br>";
     }elseif(!preg_match("/YZ+\d{8}$/i",$_POST["id"])){
-      echo "IDはYZ＋8桁の数字で入力してください";
+      echo "IDはYZ＋8桁の数字で入力してください<br>";
+    }
+    //姓のチェック
+    if(empty($_POST['lastname'])){
+      echo "姓は必ず入力してください<br>";
+    }elseif($ln_len > 25){
+      echo "姓は25文字以内で入力してください<br>";
+    }
+    //名のチェック
+    if(empty($_POST['firstname'])){
+      echo "姓は必ず入力してください<br>";
+    }elseif($fn_len > 25){
+      echo "姓は25文字以内で入力してください<br>";
+    }
+    //所属セクションのチェック
+    if(empty($_POST['section'])){
+      echo "所属セクションは必ず入力してください<br>";
+    }elseif($_POST['section'] != "シス開" && $_POST['section'] != "ビジソル" && $_POST['section'] != "サビ開"){
+      echo "シス開かビジソルかサビ開を選択してください<br>";
+    }
+    //メールアドレスのチェック
+    if(empty($_POST['mailaddress'])){
+      echo "メールアドレスは必ず入力してください<br>";
+    }elseif($mail_len > 256){
+      echo "文字数は256文字以内で入力してください<br>";
+    }elseif(!preg_match("/[!#-9A-~]+@+[a-z0-9]+.+[^.]$/i",$_POST["mailaddress"])){
+      echo "メールアドレスを正しく入力してください<br>";
+    }
+    //性別のチェック
+    if(empty($_POST['sex'])){
+      echo "性別は必ず入力してください<br>";
+    }elseif($_POST['sex'] != "男性" && $_POST['sex'] != "女性"){
+      echo "男性か女性を選択してください<br>";
     }
   }
 ?>
@@ -15,7 +57,7 @@
 <html lang="ja">
 <link rel="stylesheet" href="style.css">
 <head>
-<!--JavaScriptで入力チェック-->
+<!-- JavaScriptで入力チェック-->
   <!-- <script type="text/javascript">
     function check(frm){
       // 必須入力のname属性
@@ -87,16 +129,17 @@
     <dt>所属セクション*</dt>
     <dd>
       <select name="section">
-        <option><span id="1">シス開</span></option>
-        <option><span id="2">ビジソル</span></option>
-        <option><span id="3">サビ開</span></option>
+      <option value="選択してください">選択してください</option>
+        <option value="シス開"><span id="1">シス開</span></option>
+        <option value="ビジソル"><span id="2">ビジソル</span></option>
+        <option value="サビ開"><span id="3">サビ開</span></option>
       </select><br>
     </dd>
     <dt>メールアドレス*</dt>
       <dd><input type="text" name="mailaddress" placeholder="taro_yaz@yaz.co.jp"><br></dd>
     <dt>性別*</dt>
-      <dd><input type="radio" name="sex">男性
-      <input type="radio" name="sex">女性<br></dd>
+      <dd><input type="radio" name="sex" value="男性">男性
+      <input type="radio" name="sex" value="女性">女性<br></dd>
       <div class="must">*必須項目</div>
   </dl>
   <input type="submit" class="button" value="登録">
